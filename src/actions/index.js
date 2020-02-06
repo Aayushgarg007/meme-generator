efbb449b11f63a4d3840c5a7d2dec9eadd23b62e
Aayushgarg007 <aayushgarg226@gmail.com>
2020-02-07 01:44:34 +0530

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+export const  RECEIVE_MEMES = 'RECEIVE_MEMES';
+export const NEW_MEME = 'NEW_MEME';
+
+function receiveMemes(json) {
+  const { memes } = json.data;
+  return {
+    type: RECEIVE_MEMES,
+    memes
+  }
+}
+
+function fetchMemesJson() {
+  return fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json());
+}
+
+// actual action creator
+export function fetchMemes() {
+  return function(dispatch) {
+    return fetchMemesJson()
+      .then(json => dispatch(receiveMemes(json)));
+  }
+} 
+
+
+export function newMeme(meme) {
+  return {
+    type: NEW_MEME,
+    meme
+  }
+}
---
+++ src/actions/secrets.js
@@ -0,0 +1,4 @@
+const username = 'VickyGarg';
+const password = 'qwerty@99';
+
+export { username, password };
